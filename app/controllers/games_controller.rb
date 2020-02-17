e6d6c03e03433f77455cad91c98fae477ce3c90a
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-17 14:59:31 -0500

    Refactored the code for he GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,7 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
-    @letters = []
-    10.times { @letters << ('A'..'Z').to_a.sample }
+    @letters = generate_grid(10)
   end
 
   def score
@@ -12,17 +11,40 @@ class GamesController < ApplicationController
     @total_score = session[:total_score] || 0
     @word = params[:word]
     @start_time = params[:start_time]
-    elapsed_time = Time.now - @start_time.to_time
-    @score = @word.length * 100 / elapsed_time.to_i
     @letters = params[:letters_as_string].split(" ")
-    url = "http://wagon-dictionary.herokuapp.com/#{@word.downcase}"
-    word_serialized = open(url).read
-    word_in_dictionary = JSON.parse(word_serialized)
-    @existing = word_in_dictionary["found"]
-    @in_the_grid = @word.upcase.chars.all? { |letter| @word.upcase.count(letter) <= @letters.count(letter) }
+    @existing = existing?(@word)
+    @in_the_grid = in_the_grid?(@word, @letters)
     if @existing && @in_the_grid
+      @score = compute_score(@word, @start_time.to_time, Time.now)
       @total_score += @score
       session[:total_score] = @total_score
     end
   end
+
+  private
+
+  def generate_grid(grid_size)
+    grid = []
+    grid_size.times { grid << ('A'..'Z').to_a.sample }
+    return grid
+  end
+
+  def existing?(word)
+    url = "http://wagon-dictionary.herokuapp.com/#{word.downcase}"
+    word_serialized = open(url).read
+    word_in_dictionary = JSON.parse(word_serialized)
+    return word_in_dictionary["found"]
+  end
+
+  def in_the_grid?(word, grid)
+    # checks whether every letter appears in the grid
+    # (each letter of the grid can only be used once)
+    return word.upcase.chars.all? { |letter| word.upcase.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, start_time, end_time)
+    # returns the score
+    return (attempt.length * 100 / (end_time - start_time)).to_i
+  end
+
 end
