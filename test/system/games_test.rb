97a963e435d6cc579b532dd8ea337100994e6424
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-17 15:36:39 -0500

    Tested if we get Congrats message when we put a and the grid contains it

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -3,7 +3,28 @@ require "application_system_test_case"
 class GamesTest < ApplicationSystemTestCase
   test "Going to /new gives us a new random grid to play with" do
     visit new_url
-    assert test: "New game"
+    assert_text "New Game"
     assert_selector ".board-letter", count: 10
+  end
+  test " Playing m gives us a message that M is not a word" do
+    visit new_url
+    fill_in "word", with: "m"
+    click_on "Play"
+    assert_text "does not seem to be a valid English word..."
+  end
+  test " Playing appear gives us a message that M is not in the grid" do
+    visit new_url
+    fill_in "word", with: "appear"
+    click_on "Play"
+    assert_text "can't be built out of"
+  end
+  test " Playing a when the grid has one gives us a Congrats message" do
+    visit new_url
+    until page.find('.board').has_content?('A')
+      visit new_url
+    end
+    fill_in "word", with: "a"
+    click_on "Play"
+    assert_text "Congratulations"
   end
 end
